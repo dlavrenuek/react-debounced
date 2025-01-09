022ad8ac2ef07cb772bf89b46e1e15f4be8e70d6
Dimitri Lavrenük <20122620+dlavrenuek@users.noreply.github.com>
2025-01-09 23:37:00 +0100

    fix default value for react 19 and below 19

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -3,12 +3,14 @@ import { useCallback, useRef } from 'react';
 const DEFAULT_TIMEOUT = 250;
 
 type Callback = () => void;
+type Timeout = ReturnType<typeof setTimeout>;
+
 export type Debounce = (callback: Callback) => void;
 
 export type UseDebounce = (timeout?: number) => Debounce;
 
 const useDebounce: UseDebounce = (timeout = DEFAULT_TIMEOUT) => {
-  const timeoutRef = useRef<ReturnType<typeof setTimeout>>(null);
+  const timeoutRef = useRef<Timeout>(0 as unknown as Timeout);
 
   return useCallback(
     (callback: Callback) => {
